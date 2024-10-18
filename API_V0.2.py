0899694a297577095d29452824eb24d94f8f2f00
Stfl-code <stephane.lavie88@gmail.com>
2024-10-18 09:44:50 +0200

    Correction predict proba

--- API_V0.2.py
+++ API_V0.2.py
@@ -13,7 +13,7 @@ import plotly.graph_objects as go
 def load_mlflow_model():
     # Remplacez le chemin par le chemin vers votre modèle MLflow
     model_path = "mlflow_model" 
-    model = mlflow.pyfunc.load_model(model_path)
+    model = mlflow.lightgbm.load_model(model_path)
     return model
 
 # Charger le masque de colonnes
